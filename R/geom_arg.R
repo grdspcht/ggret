b464196e3c18dc8bee5653dbb19a0d2c261528bc
grdspcht <gerd.specht@uni-jena.de>
2023-02-08 17:23:53 +0100

    man update

--- R/geom_arg.R
+++ R/geom_arg.R
@@ -1,5 +1,33 @@
+#' Adds tree-based network layer
 #' @importFrom ggplot2
 #' @import ggplot2
+#' @import ggtree
+#' @param arg A tree-based network
+#' @return ARG plot
+#' @examples
 
-# adds tree-based network layer
+ggarg <- function(arg){
+  plot <- ggplot(arg) + geom_tree()  + geom_tiplab()
+  ret <- as.data.frame(arg$reticulation)
+  fromx <- c()
+  fromy <- c()
+  tox <- c()
+  toy <- c()
 
+  # Get coordinates for every reticulation
+  for(i in 1:nrow(ret)){
+    from <- ret[i,1]
+    to <- ret[i,2]
+    fromx <- c(fromx, plot$data$x[which(plot$data$node == from)])
+    fromy <- c(fromy, plot$data$y[which(plot$data$node == from)])
+    tox <- c(tox, plot$data$x[which(plot$data$node == to)])
+    toy <- c(toy, plot$data$y[which(plot$data$node == to)])
+
+  }
+
+  ret <- cbind(ret, fromx, fromy, tox, toy)
+
+  # Plot reticulations and backbone tree
+  plot = plot + geom_segment(data = ret, aes(x = fromx, y=fromy, xend=tox, yend=toy, colour = "red" ))
+  return(plot)
+}
