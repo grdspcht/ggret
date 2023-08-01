04a29043d442aa889b44586c6a29e156c5c0f71f
grdspcht <gerd.specht@uni-jena.de>
2023-08-01 08:37:10 +0200

    changed

--- R/readbeast.R
+++ R/readbeast.R
@@ -1,6 +1,7 @@
 library(treeio)
 library(ape)
 library(tibble)
+library(stringr)
 
 clean.colnames <- function(colnames){
   clean <- gsub("^\\[&|, |=$", "", colnames)
@@ -10,28 +11,47 @@ clean.colnames <- function(colnames){
 
 get.colnames <- function (treeTexts){
   extractedCols<- str_extract_all(treeTexts, "\\[&(\\w+)=|, (.*?)=")
-  extrUni <- unique(unlist(extr))
+  extrUni <- unique(unlist(extractedCols))
   return(extrUni)
 }
 
 file <- normalizePath("../Data/HVB_upd_bact_max10_new.summary.acg")
-
 treefile <- readLines(file)
 
 treesIndex <- grep("tree .* = ", treefile)
 treeTexts <- treefile[treesIndex]
 #treeNames <- sub("^tree (.*) = ",  "", treeTexts, invert= TRUE)
 
+# get trees
 treeTexts <- sub("^tree (.*) = ",  "", treeTexts)
 
 phylo <- read.enewick2(text = treeTexts)
 
+# add temporary node and tip labels to network if needed
+if(any(phylo$node.label == "")){
+  emptyNodes <- which(phylo$node.label == "")
+  phylo$node.label[emptyNodes] <- paste0("Node", seq(1:length(emptyNodes)))
+}
+
+if(any(phylo$tip.label == "")){
+  emptyTips <- which(phylo$tip.label == "")
+  phylo$tip.label[emptyTips] <- paste0("Tip", seq(1:length(emptyTips)))
+}
+pwl <- write.evonet(phylo)
+phyLabel <- read.enewick2(text=pwl)
+
+# Get metadata attributes (column names)
 extrdCols <- get.colnames(treeTexts)
 cleanCols <- clean.colnames(extrdCols)
 
-nodesindex <- str_extract_all(treeTexts, "[^[:alnum:]]([[:alnum:]]+)\\[&|[^[:alnum:]](#[[:alnum:]]+)\\[&")
+
+nodesindex <- str_extract_all(pwl, "([[:alnum:]]+):|(#[[:alnum:]]+):")
 nodesindex <- unlist(nodesindex)
-nodesindex <- gsub(pattern = "^[\\(\\)\\,]|\\[&$","", nodesindex) # Get rid of any leading brackets or commas, also remove trailing "[&"
+
+
+# returns a vector of the positions of
+labels <-  c(phyLabel$tip.label, phyLabel$node.label)
+ordered <- match(nodesindex,labels )
 
 dynlist <- list()
 for (col in cleanCols){
@@ -40,8 +60,9 @@ for (col in cleanCols){
 
 for (node in nodesindex){
   if(grepl("#", node)){
-
+    next
   }else
   searchstring <- paste0(node, "\\[.*?\\]")
-  str_extract_all(treeTexts, searchstring)
+  nodedata <- str_extract_all(treeTexts, searchstring)
 }
+
