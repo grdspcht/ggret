df1bcbed14d3b1f7f0fdba187a92ee25face6f32
grdspcht <gerdspecht1995@gmail.com>
2024-07-01 09:54:12 +0200

    ggret wrapper function

---
+++ R/ggret.R
@@ -0,0 +1,44 @@
+#' Plot reticulation networks
+#'
+#' @param data either treedata or evonet objects
+#' @param mapping aesthetic mappings
+#' @param retcol reticulation edge colour
+#' @param arrows if TRUE reticulation edges end with arrows
+#' @param retlinetype line type aesthetic for reticulations
+#' @param rettype Either 'straight' or 'snake'. Determines in which style reticulations are drawn
+#' @param na.rm supresses NA value warnings
+#' @param ... additional parameter
+#'
+#' @return reticulation network plot
+#' @export
+#'
+#' @examples
+ggret <- function(data,
+                  mapping = NULL,
+                  retcol = "black",
+                  arrows = FALSE,
+                  retlinetype = 2,
+                  rettype = "snake",
+                  na.rm = TRUE,
+                  ...){
+  if(is(data, "evonet") || (is(data, "evonet"))){
+    if (is.null(mapping)) {
+      mapping <- aes_(~x, ~y)
+    } else {
+      mapping <- modifyList(aes_(~x, ~y), mapping)
+    }
+    p <- ggplot(data, mapping = mapping,  retcol = retcol,
+                arrows = arrows,
+                retlinetype = retlinetype,
+                rettype = rettype, ...)
+    p <- p + geom_ret()
+    p <- p + theme_tree()
+    class(p) <- c("ggret", "ggtree", class(p))
+    return(p)
+  }else{
+    stop("ggret requires an evonet or treedata object.")
+  }
+}
+
+
+
